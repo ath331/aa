97e3237ea3306d207b92af3b557cc09e40e67845
AnTaeHwan <kstnftk@naver.com>
2019-05-01 21:17:48 +0900

    -

--- Tcp.ip_Client/Tcp.ip_Client/Client.cpp
+++ Tcp.ip_Client/Tcp.ip_Client/Client.cpp
@@ -7,7 +7,7 @@
 #define BUF_SIZE 100
 #define NAME_SIZE 20
 
-unsigned WINAPI SendMsg(void * arg);//ÈÄ;;; ±êÇãºê ³¯¾Æ°¥»·?
+unsigned WINAPI SendMsg(void * arg);
 unsigned WINAPI RecvMsg(void * arg);
 void ErrorHandling(const char * msg);
 
