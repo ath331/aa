4a37e505a3afd2b645d951cd1f6f68833e52525f
root <root@DESKTOP-158M45L.localdomain>
2019-06-24 22:47:22 +0900

    클라이언트.h의 구조체 접근 오류뜨는중

--- Tcp_ip_Server/server_class.cpp
+++ Tcp_ip_Server/server_class.cpp
@@ -3,10 +3,8 @@
 #include "./header/server_class.h"
 #include "./header/client_class.h"
 
-const static int  BUF_SIZE = 100;
+const static int BUF_SIZE = 100;
 const static int MAX_CLNT= 256;
-
-Client client;
 
 Server::Server(char* port)
 	{
@@ -32,19 +30,21 @@ void Server::sc_accept()
 		pthread_mutex_init(&mutx, nullptr );
 
 		 while(1)
-		{	
+		{
+			Client *client = new Client;	
 			clnt_adr_sz = sizeof(clnt_adr);
 			clnt_sock = accept(serv_sock, (struct sockaddr*) &clnt_adr, (socklen_t *)&clnt_adr_sz);
-			Client::Arg arg = {this, clnt_sock};
+			client->Arg arg{nullptr,clnt_sock};
 
 			pthread_mutex_lock(&mutx);
-			client.CS.push_back(clnt_sock);
+			(*client).CS.push_back(clnt_sock);
 			pthread_mutex_unlock(&mutx);
 
-	                pthread_create(&t_id, nullptr, client.handle_clnt_t, reinterpret_cast<void*>(&arg));
+	                pthread_create(&t_id, nullptr, client->handle_clnt_t, reinterpret_cast<void*>(&arg));
 			pthread_detach(t_id);	
 			printf("Connected clinet IP : %s \n", inet_ntoa(clnt_adr.sin_addr));
 		}
+		 delete client;
 		 close(serv_sock);
 
 	}
